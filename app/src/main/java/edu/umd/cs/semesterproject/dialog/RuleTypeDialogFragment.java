fb112afa489c555abcda2d25f5ee42c4f51c20f3
john-mcdonald-umd <johnmcd@umd.edu>
2017-04-24 20:54:59 -0400

    Added RuleTypeDialogFragment and implemented it

--- app/src/main/java/edu/umd/cs/semesterproject/dialog/RuleTypeDialogFragment.java
+++ app/src/main/java/edu/umd/cs/semesterproject/dialog/RuleTypeDialogFragment.java
@@ -4,31 +4,26 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import edu.umd.cs.semesterproject.R;
 
-public class RuleTypeDialog extends DialogFragment implements View.OnClickListener {
+public class RuleTypeDialogFragment extends DialogFragment implements View.OnClickListener {
 
     private TextView mTimeRuleTextView;
     private TextView mLocationRuleTextView;
 
-    public RuleTypeDialog() {
+    public RuleTypeDialogFragment() {
     }
 
-    public static RuleTypeDialog newInstance() {
-        RuleTypeDialog fragment = new RuleTypeDialog();
+    public static RuleTypeDialogFragment newInstance() {
+        RuleTypeDialogFragment ruleTypeDialogFragment = new RuleTypeDialogFragment();
 
-        return fragment;
+        return ruleTypeDialogFragment;
     }
 
     @NonNull
--- app/src/main/java/edu/umd/cs/semesterproject/fragment/VolumeFragment.java
+++ app/src/main/java/edu/umd/cs/semesterproject/fragment/VolumeFragment.java
@@ -1,7 +1,7 @@
 package edu.umd.cs.semesterproject.fragment;
 
-
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,11 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.umd.cs.semesterproject.R;
+import edu.umd.cs.semesterproject.dialog.RuleTypeDialogFragment;
 
-public class VolumeFragment extends Fragment {
+public class VolumeFragment extends Fragment implements View.OnClickListener {
 
     private static final String TITLE = "Volume";
+
     private RecyclerView mRuleRecyclerView;
+    private FloatingActionButton mFloatingActionButton;
 
     public VolumeFragment() {}
 
@@ -38,6 +41,20 @@ public class VolumeFragment extends Fragment {
         mRuleRecyclerView = (RecyclerView) view.findViewById(R.id.ruleRecyclerView);
         mRuleRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        mFloatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab);
+        mFloatingActionButton.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.fab:
+                RuleTypeDialogFragment.newInstance().show(getActivity().getSupportFragmentManager(), "RULE_TYPE_DIALOG");
+                break;
+        }
     }
 }
