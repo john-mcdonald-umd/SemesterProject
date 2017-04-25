39537629cc65f74b7c6bde8b33af57d583f5977d
JamesGaitan2018 <jgaitan@umd.edu>
2017-04-24 20:22:18 -0400

    Small change to VolumeFragment, just implemented the RecyclerView

--- app/src/main/java/edu/umd/cs/semesterproject/fragment/VolumeFragment.java
+++ app/src/main/java/edu/umd/cs/semesterproject/fragment/VolumeFragment.java
@@ -3,6 +3,8 @@ package edu.umd.cs.semesterproject.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import edu.umd.cs.semesterproject.R;
 public class VolumeFragment extends Fragment {
 
     private static final String TITLE = "Volume";
+    private RecyclerView mRuleRecyclerView;
 
     public VolumeFragment() {}
 
@@ -27,8 +30,14 @@ public class VolumeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_actions, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_rule, container, false);
+
+        mRuleRecyclerView = (RecyclerView) view.findViewById(R.id.ruleRecyclerView);
+        mRuleRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
     }
 }
