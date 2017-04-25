3a348be047c31431527c431c3d875e0137481f1b
mark-bedanov <mbedanov@gmail.com>
2017-04-24 20:10:40 -0400

    Deleted unnecessary files

--- app/src/main/java/edu/umd/cs/semesterproject/fragment/VolumeFragment.java
+++ app/src/main/java/edu/umd/cs/semesterproject/fragment/VolumeFragment.java
@@ -29,6 +29,6 @@ public class VolumeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_actions, container, false);
+        return inflater.inflate(R.layout.fragment_volume, container, false);
     }
 }
