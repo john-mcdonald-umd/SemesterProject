f413c4c0d84edeb77b2fa7c8e008ead90bfb7087
john-mcdonald-umd <johnmcd@umd.edu>
2017-05-08 22:40:13 -0400

    Added MainActivity

--- app/src/main/java/edu/umd/cs/semesterproject/activity/MainActivity.java
+++ app/src/main/java/edu/umd/cs/semesterproject/activity/MainActivity.java
@@ -1,26 +1,27 @@
-package edu.umd.cs.semesterproject;
+package edu.umd.cs.semesterproject.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import edu.umd.cs.semesterproject.R;
 import edu.umd.cs.semesterproject.adapter.ViewPagerAdapter;
 import edu.umd.cs.semesterproject.fragment.BluetoothFragment;
 import edu.umd.cs.semesterproject.fragment.VolumeFragment;
 import edu.umd.cs.semesterproject.fragment.WifiFragment;
 import edu.umd.cs.semesterproject.service.impl.TimeBackgroundService;
 
-// Main activity of the Project. Has the TabLayout which is the main UI
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    // basic fields for increments for the alarm to check time intervals
-    private final int ONE_MINUTE = 60000;
-    private final int TWO_MINUTES = 120000;
-    private final int FIVE_MINUTES = 300000;
-    private final int TEN_MINUTES = 600000;
+    private static final int ONE_MINUTE = 60000;
+    private static final int TWO_MINUTES = 120000;
+    private static final int FIVE_MINUTES = 300000;
+    private static final int TEN_MINUTES = 600000;
 
     private Toolbar mToolbar;
     private TabLayout mTabLayout;
@@ -29,16 +30,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        // Start alarm to start checking time rules
         TimeBackgroundService.setTimeAlarm(this, ONE_MINUTE);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
-        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPagerAdapter.addFragment(VolumeFragment.newInstance(), VolumeFragment.getTitle());
         viewPagerAdapter.addFragment(WifiFragment.newInstance(), WifiFragment.getTitle());
@@ -46,12 +45,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mViewPager.setAdapter(viewPagerAdapter);
 
-        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
         mTabLayout.setupWithViewPager(mViewPager);
     }
 
     @Override
-    public void onClick(View v) {
+    public boolean onCreateOptionsMenu(Menu menu) {
 
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.settings:
+                // TODO: Start settings activity.
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
+
